ef7ac14252c85ea8d647ce203699db64fd8dc483
AYDcloud <ayushdalela4903@gmail.com>
2025-03-10 00:37:59 +0530

    Add files via upload

---
+++ STEP program/Control Flow/Java Flow statements/L3/ArmstrongNumber.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ArmstrongNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        int originalNum = num, sum = 0;
+        while (originalNum > 0) {
+            int digit = originalNum % 10;
+            sum += digit * digit * digit;
+            originalNum /= 10;
+        }
+        System.out.println(num + (sum == num ? " is an Armstrong Number" : " is not an Armstrong Number"));
+    }
+}
